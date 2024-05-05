2f40b1e473088cd2344a397b72260cced0574f71
San1337 <165550888+san1337@users.noreply.github.com>
2024-05-05 12:36:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QListWidget, QVBoxLayout, QHBoxLayout,QFileDialog
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QListWidget, QVBoxLayout, QHBoxLayout
 import random 
 
 app = QApplication([])
@@ -14,14 +14,16 @@ sb = 0
 
 okno = QLabel('Добро пожаловать, в игру:Камень,ножницы, бумага')
 schet = QLabel('Счёт: ' + str(st))
-schet2 =QLabel('Счёт соперника : ' + str(sb))
+schet2 = QLabel('Счёт соперника : ' + str(sb))
+magas = QPushButton('Магазин:')
 
 
 
 bum_btn = QPushButton('бумага')
 nsh_btn =  QPushButton('ножницы')
 kam_btn = QPushButton('камень')
-
+kir_btn = QPushButton('кирка')
+kir_btn.hide()
 
 
 
@@ -37,8 +39,14 @@ v_line2 = QVBoxLayout()
 btns_line.addWidget(bum_btn)
 btns_line.addWidget(nsh_btn)
 btns_line.addWidget(kam_btn)
+btns_line.addWidget(kir_btn)
+
 v_line2.addWidget(okno)
 v_line1.addWidget(schet)
+v_line1.addWidget(schet2)
+v_line1.addWidget(magas)
+
+
 
 
 
@@ -61,19 +69,14 @@ def paper():
         okno.setText('Выбор компьютера: ' + rand + '. Ваш выбор: бумага. Вы победили!')
         st+=1
         schet.setText('Счёт: ' + str(st))
+        schet2.setText('Счёт соперника: ' + str(sb))
     if rand == 'Ножницы':
         okno.setText('Выбор компьютера: ' + rand + '. Ваш выбор: бумага. Вы проиграли!')
         sb+=1
         schet.setText('Счёт: ' + str(st))
+        schet2.setText('Счёт соперника: ' + str(sb))
     if rand == 'Бумага':
         okno.setText('Выбор компьютера: ' + rand + '. Ваш выбор: бумага. У вас с соперником ничья!')
-
-
-        
-
-
-
-
 
 
 
@@ -81,20 +84,101 @@ def paper():
 bum_btn.clicked.connect(paper)
 
 
+
+
+
 def  scissors():
+    global st
+    global sb
     rand = random.choice(list_play)
-    okno.setText('Выбор компьютера: ' + rand + '. Ваш выбор: ножницы')
+    print(rand)
+    if rand == 'Бумага':
+        okno.setText('Выбор компьютера: ' + rand + '. Ваш выбор: ножницы. Вы победили!')
+        st+=1
+        schet.setText('Счёт: ' + str(st))
+        schet2.setText('Счёт соперника: ' + str(sb))
+    if rand == 'Камень':
+        okno.setText('Выбор компьютера: ' + rand + '. Ваш выбор: ножницы. Вы проиграли!')
+        sb+=1
+        schet.setText('Счёт: ' + str(st))
+        schet2.setText('Счёт соперника: ' + str(sb))
+    if rand == 'Ножницы':
+        okno.setText('Выбор компьютера: ' + rand + '. Ваш выбор: ножницы. У вас с соперником ничья!')
 
 
 nsh_btn.clicked.connect(scissors)
 
+
 def stone():
+    global st
+    global sb
     rand = random.choice(list_play)
-    okno.setText('Выбор компьютера: ' + rand + '. Ваш выбор: камень')
+    if rand == 'Ножницы':
+        okno.setText('Выбор компьютера: ' + rand + '. Ваш выбор: камень. Вы победили!')
+        st+=1
+        schet.setText('Счёт: ' + str(st))
+        schet2.setText('Счёт соперника: ' + str(sb))
+    if rand == 'Бумага':
+        okno.setText('Выбор компьютера: ' + rand + '. Ваш выбор: камень. Вы проиграли!')
+        sb+=1
+        schet.setText('Счёт: ' + str(st))
+        schet2.setText('Счёт соперника: ' + str(sb))
+    if rand == 'Камень':
+        okno.setText('Выбор компьютера: ' + rand + '. Ваш выбор: камень. У вас с соперником ничья!')
 
 
 kam_btn.clicked.connect(stone)
 
+
+def kirka():
+    global st
+    global sb
+    rand = random.choice(list_play)
+    if rand == 'Ножницы':
+        okno.setText('Выбор компьютера: ' + rand + '. Ваш выбор: кирка. Вы победили!')
+        st+=1
+        schet.setText('Счёт: ' + str(st))
+        schet2.setText('Счёт соперника: ' + str(sb))
+    if rand == 'Бумага':
+        okno.setText('Выбор компьютера: ' + rand + '. Ваш выбор: кирка. Вы проиграли!')
+        sb+=1
+        schet.setText('Счёт: ' + str(st))
+        schet2.setText('Счёт соперника: ' + str(sb))
+    if rand == 'Камень':
+        okno.setText('Выбор компьютера: ' + rand + '. Ваш выбор: кирка. Вы победили')
+
+
+kir_btn.clicked.connect(kirka)
+
+win2 = QWidget()
+
+
+kir=QLabel('Кирка: стоит 10 очков')
+kyp=QPushButton ('Купить')
+
+kir_line = QVBoxLayout()
+kir_line.addWidget(kir)
+kir_line.addWidget(kyp)
+
+win2.setLayout(kir_line )
+
+
+
+
+
+def magaz():
+    win2.show()
+
+
+magas.clicked.connect(magaz)
+
+def buy_kir():
+    global st
+    if st >= 10:
+        st -= 10
+        schet.setText('Счет: ' + str(st))
+        kir_btn.show()
+kyp.clicked.connect(buy_kir)
 
 
 win.show()
